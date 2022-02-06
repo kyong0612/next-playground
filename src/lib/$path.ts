158714548ba3339ea6059a2c540cd745602927f2
kimkiyong <fenderst0612@gmail.com>
2022-02-06 16:46:03 +0900

    feat: add pathpid & MUI

---
+++ src/lib/$path.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// prettier-ignore
+export const pagesPath = {
+  sample: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sample' as const, hash: url?.hash })
+  },
+  web_cam: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/web-cam' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+// prettier-ignore
+export type PagesPath = typeof pagesPath
