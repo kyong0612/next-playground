ffeb0388b8917366fde4dfd0cc7c4ca4c48469db
kimkiyong <fenderst0612@gmail.com>
2022-02-12 18:15:21 +0900

    add: timer

--- src/components/templates/Timer.tsx
+++ src/components/templates/Timer.tsx
@@ -4,38 +4,17 @@ import CardContent from "@mui/material/CardContent";
 import { Button } from "@mui/material";
 import { Typography } from "@mui/material";
 import { getPrimes } from "lib/math-tools";
+import useTimer from "hooks/use-timer";
 
 const Timer: VFC<{ limit: number }> = ({ limit }) => {
-  const [timeLeft, setTimeLeft] = useState(limit);
-  const timerId = useRef<NodeJS.Timeout>();
-  const primes = useMemo(() => getPrimes(limit), [limit]);
-  const tick = (): void => setTimeLeft((t) => t - 1);
-
-  const clearTimer = () => {
-    if (timerId.current) clearInterval(timerId.current);
-  };
-
-  const reset = useCallback(() => {
-    clearTimer();
-    timerId.current = setInterval(tick, 1000);
-    setTimeLeft(limit);
-  }, [limit]);
-
-  useEffect(() => {
-    const timerId = setInterval(tick, 1000);
-    return () => reset();
-  }, [reset]);
-
-  useEffect(() => {
-    if (timeLeft === 0) reset();
-  }, [timeLeft, reset]);
+  const [timeLeft, isPrime, reset] = useTimer(limit);
 
   return (
     <Card>
       <a>Counter</a>
       <CardContent>
         <Typography>time</Typography>
-        <Typography color={primes.includes(timeLeft) ? "error" : "primary"}>
+        <Typography color={isPrime ? "error" : "primary"}>
           {timeLeft}
         </Typography>
         <Button onClick={reset}>RESET</Button>
